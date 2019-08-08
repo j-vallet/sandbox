bb9e61d4e13d87dccdc041a31ad79dd75c578285
Josselin VALLET <josselin.vallet@onesys.fr>
2019-08-08 09:30:42 +0200

    Version batch

--- getContracts.cs
+++ getContracts.cs
@@ -6,13 +6,9 @@ public IEnumerable<Contract> GetContracts(long idOrganization, IEnumerable<ICont
 
     var results = new List<Contract>();
     int batchSize = 300;
-    int currentValue = 0;
-
-    // Restrictions
-    var disjunction = Restrictions.Disjunction();
 
     // Regroupement par intérimaire
-    foreach (var cqResources in contractWeekQueryables
+    foreach (var chunk in contractWeekQueryables
         .GroupBy(cq => new 
         {
             cq.SiteId,
@@ -22,61 +18,51 @@ public IEnumerable<Contract> GetContracts(long idOrganization, IEnumerable<ICont
             cq.ResourceExternalId,
             cq.ResourceLegalName,
             cq.ResourceGivenName
-        }))
+        })
+        .Batch(batchSize))
     {
-        var contractConjunction = GetContractQueryableConjunction(cqResources.First(), () => supplierResourceAlias, () => supplierOrgResourceAlias);
+        // Restrictions
+        var disjunction = Restrictions.Disjunction();
 
-        // Si la weekDate est renseignée on prend la période de la semaine
-        // attention pour les line de type JOUR/NUI le weekdate n'est pas renseigné = null
-        // on va prendre automatiquement la semaine
-        // Sinon les dates début/fin
-        (DateTime StartDate, DateTime EndDate) WeekPeriod(IContractWeekQueryable cwq)
-            => cwq.WeekDate.HasValue
-                ? cwq.WeekDate.Value.GetTruncatedWeek().StartAndEndDate()
-                : cwq.StartDate.Value.FirstDayOfWeek().GetTruncatedWeek().StartAndEndDate();
-
-        contractConjunction.Add(
-            cqResources
-                .Select(WeekPeriod)
-                .Aggregate()
-                .Select(dr => Restrictions.Where<Contract>(c =>
-                    dr.EndDate >= c.StartDate
-                    && dr.StartDate <= c.DisplayEndDate
-                )).Disjunction()
-            );
-
-        disjunction.Add(contractConjunction);
-
-
-        if (++currentValue >= batchSize)
+        foreach (var cqResources in chunk)
         {
-            currentValue = 0;
-            var query = QueryOver(() => contractAlias)
-                .Fetch(c => c.LastAssignment).Eager
-                .Fetch(c => c.LastAssignment.Sector).Eager
-                .Fetch(c => c.LastAssignment.OrgJobSheet).Eager
-                .Fetch(c => c.LastAssignment.OrgShift).Eager
-                .Fetch(c => c.Assignments).Eager
-                .JoinAlias(x => x.SupplierOrgResource, () => supplierOrgResourceAlias)
-                .JoinAlias(() => supplierOrgResourceAlias.SupplierResource, () => supplierResourceAlias)
-                .Where(x => x.Organization.Id == idOrganization)
-                .Where(disjunction);
+            var contractConjunction = GetContractQueryableConjunction(cqResources.First(), () => supplierResourceAlias, () => supplierOrgResourceAlias);
 
-            results.AddRange(query.TransformUsing(Transformers.DistinctRootEntity).List());
-            disjunction = Restrictions.Disjunction();
+            // Si la weekDate est renseignée on prend la période de la semaine
+            // attention pour les line de type JOUR/NUI le weekdate n'est pas renseigné = null
+            // on va prendre automatiquement la semaine
+            // Sinon les dates début/fin
+            (DateTime StartDate, DateTime EndDate) WeekPeriod(IContractWeekQueryable cwq)
+                => cwq.WeekDate.HasValue
+                    ? cwq.WeekDate.Value.GetTruncatedWeek().StartAndEndDate()
+                    : cwq.StartDate.Value.FirstDayOfWeek().GetTruncatedWeek().StartAndEndDate();
+
+            contractConjunction.Add(
+                cqResources
+                    .Select(WeekPeriod)
+                    .Aggregate()
+                    .Select(dr => Restrictions.Where<Contract>(c =>
+                        dr.EndDate >= c.StartDate
+                        && dr.StartDate <= c.DisplayEndDate
+                    )).Disjunction()
+                );
+
+            disjunction.Add(contractConjunction);
         }
-    }
-    var query2 = QueryOver(() => contractAlias)
-                .Fetch(c => c.LastAssignment).Eager
-                .Fetch(c => c.LastAssignment.Sector).Eager
-                .Fetch(c => c.LastAssignment.OrgJobSheet).Eager
-                .Fetch(c => c.LastAssignment.OrgShift).Eager
-                .Fetch(c => c.Assignments).Eager
-                .JoinAlias(x => x.SupplierOrgResource, () => supplierOrgResourceAlias)
-                .JoinAlias(() => supplierOrgResourceAlias.SupplierResource, () => supplierResourceAlias)
-                .Where(x => x.Organization.Id == idOrganization)
-                .Where(disjunction);
 
-    results.AddRange(query2.TransformUsing(Transformers.DistinctRootEntity).List());
+        var query = QueryOver(() => contractAlias)
+            .Fetch(c => c.LastAssignment).Eager
+            .Fetch(c => c.LastAssignment.Sector).Eager
+            .Fetch(c => c.LastAssignment.OrgJobSheet).Eager
+            .Fetch(c => c.LastAssignment.OrgShift).Eager
+            .Fetch(c => c.Assignments).Eager
+            .JoinAlias(x => x.SupplierOrgResource, () => supplierOrgResourceAlias)
+            .JoinAlias(() => supplierOrgResourceAlias.SupplierResource, () => supplierResourceAlias)
+            .Where(x => x.Organization.Id == idOrganization)
+            .Where(disjunction);
+
+        results.AddRange(query.TransformUsing(Transformers.DistinctRootEntity).List());
+    }
+    
     return results;
 }
